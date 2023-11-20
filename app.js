049ba26fc9bf0c8bdd78250dd431fa10d384915a
bengardner1 <bengardner433@gmail.com>
2023-11-19 22:00:30 -0500

    done?

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import Hello from './hello.js';
-import Lab5 from './lab5.js';
-import cors from "cors";
+import express from "express";
+import Lab5 from "./lab5.js";
 import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./Modules/routes.js";
+import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
+ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
-Hello(app);
-app.listen(4000); 
+app.listen(4000);
