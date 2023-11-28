7aa3be9676cfef9eb6bd66f86029fa69c12edd4a
bengardner1 <bengardner433@gmail.com>
2023-11-28 15:02:42 -0500

    done with configuring server for now

--- app.js
+++ app.js
@@ -6,9 +6,19 @@ import cors from "cors";
 import UserRoutes from "./users/routes.js";
 import "dotenv/config";
 import mongoose from "mongoose";
+import session from "express-session";
 mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:3000",}));
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+app.use(session(sessionOptions));
+         
 app.use(express.json());
 UserRoutes(app);
 ModuleRoutes(app);
