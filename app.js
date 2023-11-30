2baa7ce89097a1333dfe1399b7979b25a2f4933c
bengardner1 <bengardner433@gmail.com>
2023-11-30 12:54:01 -0500

    trying to get atlas to work

--- app.js
+++ app.js
@@ -6,9 +6,30 @@ import cors from "cors";
 import UserRoutes from "./users/routes.js";
 import "dotenv/config";
 import mongoose from "mongoose";
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+import session from "express-session";
+const CONNECTION_STRING = "mongodb+srv://bengardner433:bladee333@cluster0.gv9wcmb.mongodb.net/?retryWrites=true&w=majority";
+//const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    })
+  );
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+app.use(session(sessionOptions));
 app.use(express.json());
 UserRoutes(app);
 ModuleRoutes(app);
